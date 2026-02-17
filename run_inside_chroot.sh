02388294395e205c7df39c497a0c298e1f779944
Thiago S <sousathiago@protonmail.com>
2026-02-16 22:07:04 -0300

    Refactor Ubiquity execution command in run_inside_chroot.sh for improved readability

--- run_inside_chroot.sh
+++ run_inside_chroot.sh
@@ -152,4 +152,6 @@ sed -i '/^NAME_REGEX=/d' /etc/adduser.conf
 echo "NAME_REGEX='^[a-z][-.a-z0-9_]*$'" >> /etc/adduser.conf
 
 # Configure Ubiquity to use the preseed file and set it to automatic mode
-sed -i "s|^Exec=.*|Exec=sudo --preserve-env=DBUS_SESSION_BUS_ADDRESS,XDG_DATA_DIRS,XDG_RUNTIME_DIR,GTK_THEME sh -c 'debconf-set-selections /cdrom/preseed/preseed.cfg && WEBKIT_DISABLE_COMPOSITING_MODE=1 ubiquity gtk_ui --automatic'|" /usr/share/applications/ubiquity.desktop
+sed -i '/^Exec=/c\
+Exec=sudo --preserve-env=DBUS_SESSION_BUS_ADDRESS,XDG_DATA_DIRS,XDG_RUNTIME_DIR,GTK_THEME sh -c '\''debconf-set-selections /cdrom/preseed/preseed.cfg  && WEBKIT_DISABLE_COMPOSITING_MODE=1 ubiquity gtk_ui --automatic'\''' \
+/usr/share/applications/ubiquity.desktop
